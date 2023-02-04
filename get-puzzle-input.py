52e05c33c69395ebbcbd0d4ed659b3d00ddfbcfb
marques-j-robinson <marques.j.robinson@gmail.com>
2023-02-04 15:01:00 -0800

    create setup_request_headers to return the github session token as a dictionary with a key of Cookie

--- get-puzzle-input.py
+++ get-puzzle-input.py
@@ -4,4 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-GITHUB_SESSION = os.getenv('GITHUB')
+def setup_request_headers():
+	session = os.getenv('GITHUB')
+	return {'Cookie':f'session={session}'}
