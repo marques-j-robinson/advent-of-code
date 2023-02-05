e457015765f9ba751599379737247bc46159396d
marques-j-robinson <marques.j.robinson@gmail.com>
2023-02-05 08:16:22 -0800

    refactor logic in main to smaller functions

--- main.py
+++ main.py
@@ -7,15 +7,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-if __name__ == '__main__':
-	cmd_args = sys.argv[1:]
-    if '--generate' in cmd_args:
+def cmd_args():
+    return sys.argv[1:]
+
+
+def generate():
+    if '--generate' in cmd_args():
         save_puzzle_input()
-    elif len(cmd_args) == 0:
+
+
+def build_module_filename():
+    event = os.getenv('EVENT')
+    day = leading_zero(os.getenv('DAY'))
+    return importlib.import_module(f'Events.{event}.day{day}')
+
+
+def solve():
+    if len(cmd_args()) == 0:
         print(f'Solution for {build_puzzle_id()}')
         puzzle_input = read_puzzle_input()
-        s_module = importlib.import_module(f'Events.{os.getenv('EVENT')}.day{leading_zero(os.getenv('DAY'))}')
+        s_module = build_module_filename()
         s = s_module.Solution(puzzle_input)
         s.solve()
-    else:
-        print('UNKNOWN CMD\nEXITING...')
+
+
+if __name__ == '__main__':
+    generate()
+    solve()
