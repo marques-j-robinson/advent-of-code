b10639f4cef34ee389add4c28eea5bac19b1c121
marques-j-robinson <marques.j.robinson@gmail.com>
2023-02-05 08:19:30 -0800

    use functions to solve part 1 and 2 instead of the class structure

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def generate():
         save_puzzle_input()
 
 
-def build_module_filename():
+def build_solution_filename():
     event = os.getenv('EVENT')
     day = leading_zero(os.getenv('DAY'))
     return importlib.import_module(f'Events.{event}.day{day}')
@@ -26,9 +26,9 @@ def solve():
     if len(cmd_args()) == 0:
         print(f'Solution for {build_puzzle_id()}')
         puzzle_input = read_puzzle_input()
-        s_module = build_module_filename()
-        s = s_module.Solution(puzzle_input)
-        s.solve()
+        s = build_solution_filename()
+        s.p1(puzzle_input)
+        s.p2(puzzle_input)
 
 
 if __name__ == '__main__':
