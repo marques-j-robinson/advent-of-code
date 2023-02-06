0e7bed8187d0cd5710f83b10500686db8928688b
marques-j-robinson <marques.j.robinson@gmail.com>
2023-02-06 15:44:13 -0800

    solve 2019_02 part 2

--- Events/2019/day02.py
+++ Events/2019/day02.py
@@ -5,10 +5,10 @@ def format_puzzle_input(puzzle_input):
     return int_list(split_by_comma(puzzle_input))
 
 
-def p1(puzzle_input):
-    puzzle_input = format_puzzle_input(puzzle_input)
-    puzzle_input[1] = 12
-    puzzle_input[2] = 2
+def program(puzzle_input, noun, verb):
+    puzzle_input = puzzle_input[:]
+    puzzle_input[1] = noun
+    puzzle_input[2] = verb
     idx = 0
     addcode = 1
     multiplycode = 2
@@ -26,11 +26,23 @@ def p1(puzzle_input):
         if opcode == multiplycode:
             puzzle_input[c] = puzzle_input[a]*puzzle_input[b]
         idx += step
-    print(puzzle_input)
     return puzzle_input[0]
+
+
+def p1(puzzle_input):
+    puzzle_input = format_puzzle_input(puzzle_input)
+    noun = 12
+    verb = 2
+    return program(puzzle_input, noun, verb)
 
 
 def p2(puzzle_input):
     res = 0
     puzzle_input = format_puzzle_input(puzzle_input)
+    check_val = 19690720
+    for noun in range(100):
+        for verb in range(100):
+            val = program(puzzle_input, noun, verb)
+            if check_val == val:
+                res = 100 * noun + verb
     return res
