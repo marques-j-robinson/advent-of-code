d202e87c0c9097ff02d3d71c595d84fae88be9b9
marques-j-robinson <marques.j.robinson@gmail.com>
2023-02-06 11:09:46 -0800

    solve 2018_02 part 2

--- Events/2018/day02.py
+++ Events/2018/day02.py
@@ -33,7 +33,25 @@ def p1(puzzle_input):
 	return two*three
 
 
+def differ_by_exactly_one_char(w1, w2):
+	if w1 == w2:
+		return False
+	else:
+		diffs = 0
+		for idx, i in enumerate(w1):
+			j = w2[idx]
+			if i != j:
+				diffs += 1
+		return diffs == 1
+
+
 def p2(puzzle_input):
-	res = 0
+	res = ''
 	puzzle_input = split_by_new_line(puzzle_input)
+	for idx, w1 in enumerate(puzzle_input):
+		for w2 in puzzle_input[idx+1:]:
+			if differ_by_exactly_one_char(w1, w2) is True:
+				for jdx, l in enumerate(w1):
+					if l == w2[jdx]:
+						res += l
 	return res
