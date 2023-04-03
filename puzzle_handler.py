447434b69469658e43c4fc8624def65f5061547d
marques-j-robinson <marques.j.robinson@gmail.com>
2023-04-03 08:13:13 -0700

    request puzzle input from the advent of code server

--- puzzle_handler.py
+++ puzzle_handler.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import urllib3
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,6 +10,9 @@ from format_string import leading_zero
 
 event = os.getenv('EVENT')
 day = os.getenv('DAY')
+headers = {'Cookie'f'session={os.getenv('TOKEN')}'}
+base_uri = 'https://adventofcode.com'
+url = f'{base_uri}/{event}/day/{day}/input'
 parser = argparse.ArgumentParser()
 parser.add_argument("-e", "--Event", help="Event ID of puzzle")
 parser.add_argument("-y", "--Year", help="(User Friendly) Event ID of puzzle")
@@ -18,7 +22,6 @@ args = parser.parse_args()
 
 class PuzzleHandler:
 	"""
-	Request puzzle input from the Advent of Code server.
 	Cache puzzle input in the file system.
 
 	Import puzzle solution module.
@@ -39,3 +42,14 @@ class PuzzleHandler:
 		self.e = e
 		self.d = d
 		self.day_with_leading_zero = leading_zero(d)
+
+	def get_puzzle_input(self):
+		"""
+		Request puzzle input from the Advent of Code server.
+		Minor formatting to the response data:
+			- Decoding the String by 'utf-8'
+			- Removing any new line characters at the end of the String.
+		"""
+		http = urllib3.PoolManager()
+		r = http.request('GET', url, headers=headers)
+		return r.data.decode('utf-8').rstrip('\n')
