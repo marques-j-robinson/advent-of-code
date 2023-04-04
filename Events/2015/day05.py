2c0efe758cae172c8568ebe6ac221af56d60d160
marques-j-robinson <marques.j.robinson@gmail.com>
2023-04-03 18:11:31 -0700

    solve part 1

---
+++ Events/2015/day05.py
@@ -0,0 +1,40 @@
+from main import CacheLayer
+
+
+vowels = 'aeiou'
+forbidden = ['ab', 'cd', 'pq', 'xy']
+
+
+def check_dup(s):
+	res = False
+	for idx, l in enumerate(s):	
+		if idx > 0 and l == s[idx-1]:
+			res = True
+	return res
+
+
+def calc_vowel_count(s):
+	res = 0
+	for l in s:
+		if l in vowels:
+			res += 1
+	return res
+
+
+def check_forbidden(s):
+	res = False
+	for f in forbidden:
+		if f in s:
+			res = True
+	return res
+
+
+class S(CacheLayer):
+
+	def solve(self):
+		self.split_by_new_line()
+		for s in self.puzzle_input:
+			if check_forbidden(s) is False:
+				if calc_vowel_count(s) >= 3:
+					if check_dup(s) is True:
+						self.p1 += 1
