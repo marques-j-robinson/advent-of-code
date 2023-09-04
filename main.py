d7a7e3ddad9f3675876e6ba778ffae4c9785aa9b
marques-j-robinson <marques.j.robinson@gmail.com>
2023-09-03 22:57:39 -0700

    update main.py, add logic for understanding how to configure the event and day puzzle_id values

--- main.py
+++ main.py
@@ -1,145 +1,19 @@
 import os
-import shutil
 import argparse
-import importlib
-
-import urllib3
-import pyperclip
 from dotenv import load_dotenv
 load_dotenv()
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--Event", help="Event ID of puzzle")
-parser.add_argument("-y", "--Year", help="(User Friendly) Event ID of puzzle")
-parser.add_argument("-d", "--Day", help="Day ID of puzzle")
+parser.add_argument("-e", "--Event", help="Event ID")
+parser.add_argument("-d", "--Day", help="Day ID")
 args = parser.parse_args()
 
 
-class UserInput:
-
-    def __init__(self, e=os.getenv('EVENT'), d=os.getenv('DAY')):
-        self.event = args.Event or args.Year or e
-        if os.path.exists(f'Events/{self.event}') is False:
-            os.mkdir(f'Events/{self.event}')
-        self.day = args.Day or d
-        self.day_with_leading_zero = str(self.day).zfill(2)
-        solution_filepath = f'Events/{self.event}/day{self.day_with_leading_zero}.py'
-        if os.path.exists(solution_filepath) is False:
-            shutil.copy('template.py', solution_filepath)
-        self.id = f'{self.event}_{self.day_with_leading_zero}'
-        self.puzzle_input = None
-        self.p1 = 0
-        self.p2 = 0
-        self.solution_module = importlib.import_module(f'Events.{self.event}.day{self.day_with_leading_zero}')
+event = args.Event or os.getenv('EVENT') or 2015
+day = args.Day or os.getenv('DAY') or 1
 
 
-class DataTranslation(UserInput):
-    
-    def init_puzzle_input(self):
-        self.puzzle_input = self.puzzle_input.decode('utf-8').rstrip('\n')
-    
-    def split_by_new_line(self):
-        self.puzzle_input = [i.strip() for i in self.puzzle_input.split("\n")]
-    
-    def split_by_comma(self):
-        self.puzzle_input = self.puzzle_input.split(', ')
-
-    def list_of_tuples(self):
-        self.puzzle_input = [(d[0], int(d[1:len(d)])) for d in self.puzzle_input]
-
-
-class CacheLayer(DataTranslation):
-    
-    def get_puzzle_input(self):
-        if self.is_cached():
-            f = open(self.cache_path(), 'r')
-            self.puzzle_input = f.read()
-            f.close()
-        else:
-            self.request()
-            f = open(self.cache_path(), 'w')
-            f.write(self.puzzle_input)
-            f.close()
-
-    def cache_path(self):
-        return f'cache/{self.id}'
-
-    def is_cached(self):
-        return os.path.exists(self.cache_path())
-
-    def set_headers(self):
-        return {'Cookie':f"session={os.getenv('TOKEN')}"}
-
-    def set_url(self):
-        return f'https://adventofcode.com/{self.event}/day/{self.day}/input'
-
-    def request(self):
-        http = urllib3.PoolManager()
-        response = http.request('GET', self.set_url(), headers=self.set_headers())
-        self.puzzle_input = response.data
-        self.init_puzzle_input()
-
-
-def int_list(a):
-    return [int(i) for i in a]
-
-
-class Grid:
-
-    def __init__(self, directions):
-        [up, down, left, right] = directions
-        self.up = up
-        self.down = down
-        self.left = left
-        self.right = right
-        self.x = 0
-        self.y = 0
-        self.save_coord()
-        self.seen = []
-        self.add_seen()
-
-    def parse_coord(self):
-        return int_list(self.coord.split(","))
-
-    def save_coord(self):
-        self.coord = f"{self.x},{self.y}"
-
-    def save_move(self, d):
-        self.move(d)
-        self.add_seen()
-
-    def move(self, d):
-        [x, y] = self.parse_coord()
-        if d == self.up:
-            y += 1
-        elif d == self.right:
-            x += 1
-        elif d == self.down:
-            y -= 1
-        elif d == self.left:
-            x -= 1
-        self.x = x
-        self.y = y
-
-    def add_seen(self):
-        self.save_coord()
-        if self.coord not in self.seen:
-            self.seen.append(self.coord)
-
-    def manhattan_distance(self):
-        return abs(self.x) + abs(self.y)
-
-
-if __name__ == '__main__':
-    i = UserInput()
-    s = i.solution_module.S()
-    s.get_puzzle_input()
-    s.solve()
-    if s.p2 == 0:
-        print(f'Part 1: {s.p1}')
-        pyperclip.copy(str(s.p1))
-    else:
-        print(f'Part 1: {s.p1}')
-        print(f'Part 2: {s.p2}')
-        pyperclip.copy(str(s.p2))
+if __name__ == "__main__":
+    print(event)
+    print(day)
