a3213287da4b1577cdec03e7a3024a310dfa1edd
marques-j-robinson <marques.j.robinson@gmail.com>
2023-09-04 13:11:22 -0700

    add module import logic with error handling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import importlib
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,8 +14,15 @@ args = parser.parse_args()
 event = args.Event or os.getenv('EVENT') or 2015
 day = args.Day or os.getenv('DAY') or 1
 day_with_leading_zero = str(day).zfill(2)
-puzzle_id = f'{event}_{day_with_leading_zero}'
+puzzle_id = f"{event}_{day_with_leading_zero}"
 
 
 if __name__ == "__main__":
-    print(puzzle_id)
+    print(f"Executing {puzzle_id} Solution...")
+    try:
+        solution_module = importlib.import_module(f"Events.{event}.day{day_with_leading_zero}")
+        solution = solution_module.Solution()
+    except Exception as e:
+        print("Exception Thrown!!")
+        print(e)
+        print("Please remember to create the Event folder as well as the solution module and class.")
