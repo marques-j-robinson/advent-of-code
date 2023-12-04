069587db42c71ad5d9262a68e1b51c2324e3b31e
marques-j-robinson <marques.j.robinson@gmail.com>
2023-12-04 14:19:21 -0800

    solve 2015 day 1 part 2

--- src/2015/1.js
+++ src/2015/1.js
@@ -8,3 +8,17 @@ const p1 = sum(input
     .replaceAll(')', '-1,')
     .split(','))
 console.log(p1)
+
+let p2 = 0
+let cur = 0
+
+const move = i => i === '(' ? cur += 1 : cur -= 1
+const isInitBasementEntry = () => cur === -1
+
+input.split('').forEach((i, position) => {
+    move(i)
+    if (p2 === 0 && isInitBasementEntry()) {
+        p2 = position+1
+    }
+})
+console.log(p2)
