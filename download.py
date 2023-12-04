579a5a90e541187784398003f8fd081e2bf49307
marques-j-robinson <marques.j.robinson@gmail.com>
2023-12-04 09:37:11 -0800

    start adding basic logic for download.py

---
+++ download.py
@@ -0,0 +1,22 @@
+import os
+
+import urllib3
+from dotenv import load_dotenv
+load_dotenv()
+
+
+e = 2015
+d = 1
+url = f'https://adventofcode.com/{e}/day/{d}/input'
+token = os.getenv('TOKEN')
+headers = {'Cookie':f'session={token}'}
+
+
+def save_new_puzzle_input():
+    http = urllib3.PoolManager()
+    r = http.request('GET', url, headers=headers)
+    return r.data.decode('utf-8').rstrip('\n')
+
+
+if __name__ == '__main__':
+    print('hi')
