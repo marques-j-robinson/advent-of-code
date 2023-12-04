ffc8080b4864701367b493afb26412bc641932a7
marques-j-robinson <marques.j.robinson@gmail.com>
2023-12-04 09:40:16 -0800

    if cache dir doesn't exist, force its creation

--- download.py
+++ download.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 e = 2015
 d = 1
+
 url = f'https://adventofcode.com/{e}/day/{d}/input'
 token = os.getenv('TOKEN')
 headers = {'Cookie':f'session={token}'}
@@ -18,5 +19,11 @@ def save_new_puzzle_input():
     return r.data.decode('utf-8').rstrip('\n')
 
 
+def check_cache_dir():
+    if not os.path.exists('cache'):
+        os.makedirs('cache')
+
+
 if __name__ == '__main__':
+    check_cache_dir()
     print('hi')
