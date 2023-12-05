a23528f8367f0ce196f7763cc8ae75db480b5af0
marques-j-robinson <marques.j.robinson@gmail.com>
2023-12-05 13:57:34 -0800

    solve for part 2 year 2015 day 2

--- js/2015/2.js
+++ js/2015/2.js
@@ -2,13 +2,17 @@ import {input, splitByLine} from '../util.js'
 import {
     arraySum,
     areaSquare,
+    perimeterSquare,
     surfaceAreaRectangularPrism,
+    volumeRectangularPrism,
 } from '../math.js'
+
+let i = splitByLine(input).map(sides => sides.split('x').map(Number))
 
 const sm = arr => {
     arr.sort((a, b) => a-b)
     return arr.slice(0, arr.length-1)
 }
 
-console.log(arraySum(splitByLine(input).map(sides => sides.split('x').map(Number))
-    .map(sides => areaSquare(sm(sides))+surfaceAreaRectangularPrism(sides))))
+console.log(arraySum(i.map(sides => areaSquare(sm(sides))+surfaceAreaRectangularPrism(sides))))
+console.log(arraySum(i.map(sides => perimeterSquare(sm(sides))+volumeRectangularPrism(sides))))
--- js/math.js
+++ js/math.js
@@ -2,4 +2,7 @@ export const arraySum = arr => arr.reduce((acc, i) => acc+Number(i), 0)
 
 export const surfaceAreaRectangularPrism = ([l, w, h]) => 2*l*w + 2*w*h + 2*h*l
 
+export const volumeRectangularPrism = ([l, w, h]) => l*w*h
+
 export const areaSquare = sides => sides.reduce((acc, i) => acc*i, 1)
+export const perimeterSquare = sides => sides.reduce((acc, i) => acc+i*2, 0)
