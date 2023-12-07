3ab5fee6c3f1c3a8252fd744356a5f01ec99d5ac
marques-j-robinson <marques.j.robinson@gmail.com>
2023-12-07 15:34:16 -0800

    refactor 2015 day 6

--- js/2015/6.js
+++ js/2015/6.js
@@ -1,16 +1,25 @@
-import {input, splitByLine} from '../util.js'
-const i = splitByLine(input)
+import {input, splitBy} from '../util.js'
+import {arraySum} from '../math.js'
+
+const part2 = true
 
 const Xs = Array(1000).fill(null)
 const Ys = Array(1000).fill(null)
-const grid = Xs.map(x => (Ys.map(y => ({on:false, bright:0}))))
+const grid = Xs.map(x => (Ys.map(y => part2 ? {on:false, bright:0} : false)))
 
-export const parseInstruction = i => {
-    const [_, cmd, start, end] = i.match(/([\s\w]*) ([\d,]*) through ([\d,]*)/)
-    return {cmd, start,end}
+const lightSwitch = (isOn, cmd) => {
+    if (cmd === 'turn on') return true
+    if (cmd === 'turn off') return false
+    if (cmd === 'toggle') return !isOn
 }
 
-export const parseCoord = coord => {
+const brightness = (i, cmd) => {
+    if (cmd === 'turn on') return i+1
+    if (cmd === 'turn off') return i === 0 ? 0 : i-1
+    if (cmd === 'toggle') return i+2
+}
+
+const getCoordValues = coord => {
     const [x, y] = coord.split(',')
     return {
         x: Number(x),
@@ -18,41 +27,34 @@ export const parseCoord = coord => {
     }
 }
 
-export const handleLightSwitch = (cmd, curVal) => {
-    if (cmd === 'turn on') return true
-    if (cmd === 'turn off') return false
-    if (cmd === 'toggle') return !curVal
+const parse = i => {
+    const [_, cmd, start, end] = i.match(/([\s\w]*) ([\d,]*) through ([\d,]*)/)
+    return {
+        cmd,
+        start: getCoordValues(start),
+        end: getCoordValues(end),
+    }
 }
 
-export const handleBrightness = (cmd, curVal) => {
-    if (cmd === 'turn on') return curVal+1
-    if (cmd === 'turn off') return curVal === 0 ? 0 : curVal-1
-    if (cmd === 'toggle') return curVal+2
-}
-
-let p1 = 0
-let p2 = 0
-
-i .forEach(i => {
-    const {cmd, start, end} = parseInstruction(i)
-    const {x:startX, y:startY} = parseCoord(start)
-    const {x:endX, y:endY} = parseCoord(end)
-    for (let x = startX; x <= endX; ++x) {
-        for (let y = startY; y <= endY; ++y) {
-            const {on, bright} = grid[x][y]
-            grid[x][y] = {
-                on: handleLightSwitch(cmd, on),
-                bright: handleBrightness(cmd, bright),
+splitBy(input, '\n').forEach(i => {
+    const {cmd, start, end} = parse(i)
+    for (let x = start.x; x <= end.x; ++x) {
+        for (let y = start.y; y <= end.y; ++y) {
+            if (!part2) grid[x][y] = lightSwitch(grid[x][y], cmd)
+            if (part2) {
+                const {on, bright} = grid[x][y]
+                grid[x][y] = {
+                    on: lightSwitch(on, cmd),
+                    bright: brightness(bright, cmd),
+                }
             }
         }
     }
 })
 
-grid.forEach(row => {
-    row.forEach(({on, bright}) => {
-        if (on) p1 += 1
-        p2 += bright
-    })
-})
-console.log(p1)
-console.log(p2)
+console.log(
+    arraySum(grid.flatMap(row => {
+        if (!part2) return row
+        if (part2) return row.map(i => i.bright)
+    }))
+)
