096ca1634d7f116879b22450c7e90e02b9ebde46
marques-j-robinson <marques.j.robinson@gmail.com>
2023-12-07 11:51:59 -0800

    fix bug in 2015 day 4 part 2 solution

--- js/2015/4.js
+++ js/2015/4.js
@@ -1,12 +1,12 @@
 import {input as secretKey} from '../util.js'
 import md5 from 'blueimp-md5'
 
-const part2 = false
+const part2 = true
 
 const mine = zeros => {
     let i = 1
     while (true) {
-        if (md5(`${secretKey}${i}`).slice(0, 5) === zeros) break
+        if (md5(`${secretKey}${i}`).slice(0, zeros.length) === zeros) break
         ++i
     }
     return i
