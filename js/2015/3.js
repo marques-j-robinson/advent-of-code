cda675118134832d54a462dc04a89d62a0eea8bc
marques-j-robinson <marques.j.robinson@gmail.com>
2023-12-07 08:24:21 -0800

    refactor 2015 day 3

--- js/2015/3.js
+++ js/2015/3.js
@@ -1,7 +1,10 @@
-import {input} from '../util.js'
-const i = input.split('')
+import {input, splitBy} from '../util.js'
 
-const createSanta = () => ({
+const part2 = true
+
+const moves = splitBy(input, '')
+
+const createGrid = () => ({
     x: 0,
     y: 0,
     seen: ['0,0'],
@@ -9,33 +12,37 @@ const createSanta = () => ({
 
 const createCoord = ({x, y}) => `${x},${y}`
 
-const move = (santa, direction) => {
-    if (direction === '>') santa.x += 1
-    if (direction === '<') santa.x -= 1
-    if (direction === '^') santa.y += 1
-    if (direction === 'v') santa.y -= 1
-    const coord = createCoord(santa)
-    if (!santa.seen.includes(coord)) {
-        santa.seen.push(coord)
+const move = (grid, direction) => {
+    if (direction === '>') grid.x += 1
+    if (direction === '<') grid.x -= 1
+    if (direction === '^') grid.y += 1
+    if (direction === 'v') grid.y -= 1
+    const coord = createCoord(grid)
+    if (!grid.seen.includes(coord)) {
+        grid.seen.push(coord)
     }
 }
 
 const removeDups = (arr1, arr2) =>
     ([...arr1,...arr2, ].filter((v, i, self) => i === self.indexOf(v)))
 
-const s = createSanta()
-i.forEach(i => move(s, i))
-console.log(s.seen.length)
+if (!part2) {
+    const g = createGrid()
+    moves.forEach(i => move(g, i))
+    console.log(g.seen.length)
+}
 
-const s1 = createSanta()
-const s2 = createSanta()
+if (part2) {
+    const s1 = createGrid()
+    const s2 = createGrid()
 
-i.forEach((i, idx) => {
-    if (idx%2===0) {
-        move(s1, i)
-    } else {
-        move(s2, i)
-    }
-})
+    moves.forEach((i, idx) => {
+        if (idx%2===0) {
+            move(s1, i)
+        } else {
+            move(s2, i)
+        }
+    })
 
-console.log(removeDups(s1.seen, s2.seen).length)
+    console.log(removeDups(s1.seen, s2.seen).length)
+}
