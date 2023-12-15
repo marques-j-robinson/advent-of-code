18cf929c442f622bd396cb0b7c0465d5b948b9bf
marques-j-robinson <marques.j.robinson@gmail.com>
2023-12-15 00:34:42 -0800

    solve 2023 day 15 part 2

--- js/2023/15.js
+++ js/2023/15.js
@@ -2,6 +2,8 @@ import {input} from '../util.js'
 import {sum} from '../array.js'
 const steps = input.split(',')
 
+const part2 = true
+
 const hash = str => {
     let total = 0
     str.split('').forEach(s => {
@@ -11,4 +13,35 @@ const hash = str => {
     return total
 }
 
-console.log(sum(steps.map(hash)))
+const parse = str => str.match(/([a-z]*)([\=\-])(\d*)/)
+
+if (!part2) console.log(sum(steps.map(hash)))
+if (part2) {
+    const boxes = {}
+    steps.forEach(step => {
+        const [_, label, op, v] = parse(step)
+        const val = Number(v)
+        const boxId = hash(label)
+        if (op === '-' && boxes[boxId]) {
+            boxes[boxId] = boxes[boxId].filter(i => i.label !== label)
+            if (boxes[boxId].length === 0) delete boxes[boxId]
+        }
+        if (op === '=') {
+            if (!boxes[boxId]) boxes[boxId] = []
+            if (boxes[boxId].filter(i => i.label === label).length === 0) {
+                boxes[boxId].push({label, val})
+            } else {
+                boxes[boxId].forEach(i => {
+                    if (i.label === label) i.val = val
+                })
+            }
+        }
+    })
+    console.log(sum(Object.keys(boxes).map(key => {
+        let total = 0
+        boxes[key].forEach(({val}, slot) => {
+            total += (1+Number(key))*(1+Number(slot))*val
+        })
+        return total
+    })))
+}
