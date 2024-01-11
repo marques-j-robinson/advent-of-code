ab7dfd0fb777bfdc6b9b907ba975e193b0c0bb0e
marques-j-robinson <marques.j.robinson@gmail.com>
2024-01-11 13:57:08 -0800

    solve 2015 day 13 part 1

---
+++ js/2015/13.js
@@ -0,0 +1,38 @@
+import {input} from '../puzzle-input.js'
+import {permute} from '../array.js'
+
+const table = {}
+
+const regex = /([a-zA-Z]*) would ([gain|lose]*) ([0-9]*) [a-z ]* to ([a-zA-Z]*)./
+function parse(instruction) {
+    const [_, name, type, amount, recipient] = regex.exec(instruction)
+    if (!table[name]) {
+        table[name] = {}
+    }
+    table[name][recipient] = type === 'gain' ? Number(amount) : -Number(amount)
+}
+
+input.split('\n').forEach(parse)
+//console.log(table)
+
+const combos = permute(Object.keys(table))
+//const combos = [permute(Object.keys(table))[0]]
+//console.log(combos)
+
+let max = 0
+combos.forEach(names => {
+    let total = 0
+    for (let i = 0; i < names.length; i++) {
+        if (i===0) {
+            const a = names[names.length-1]
+            const b = names[i]
+            total += table[a][b]+table[b][a]
+        } else {
+            const a = names[i-1]
+            const b = names[i]
+            total += table[a][b]+table[b][a]
+        }
+    }
+    if (total > max) max = total
+})
+console.log(max)
