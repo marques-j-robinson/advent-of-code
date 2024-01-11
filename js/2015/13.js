ff72bc90688f176f6a4fcecdd43431875ff3a296
marques-j-robinson <marques.j.robinson@gmail.com>
2024-01-11 14:11:55 -0800

    solve 2015 day 13 part 2

--- js/2015/13.js
+++ js/2015/13.js
@@ -1,6 +1,8 @@
 import {input} from '../puzzle-input.js'
 import {permute} from '../array.js'
 
+const part2 = true
+
 const table = {}
 
 const regex = /([a-zA-Z]*) would ([gain|lose]*) ([0-9]*) [a-z ]* to ([a-zA-Z]*)./
@@ -13,6 +15,15 @@ function parse(instruction) {
 }
 
 input.split('\n').forEach(parse)
+
+if (part2) {
+    const names = Object.keys(table)
+    table['self'] = {}
+    names.forEach(n => {
+        table[n]['self'] = 0
+        table['self'][n] = 0
+    })
+}
 
 const combos = permute(Object.keys(table))
 
