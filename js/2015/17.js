48727804b4186edd4b4b2a79574cf38989b0024b
marques-j-robinson <marques.j.robinson@gmail.com>
2024-01-20 04:27:02 -0800

    solve 2015 day 17 part 2

--- js/2015/17.js
+++ js/2015/17.js
@@ -1,13 +1,28 @@
 import {input} from '../puzzle-input.js'
 import {sum, combination} from '../array.js'
 
+const part2 = true
+
 const containers = input.split('\n').map(i => Number(i))
 
 const amountOfEggnog = 150
 
+let min = containers.length
 let total = 0
 for (let n of combination(containers)) {
-    if (sum(n) === amountOfEggnog) total += 1
+    if (sum(n) === amountOfEggnog) {
+        if (!part2) {
+            total += 1
+        } else {
+            if (n.length < min) {
+                min = n.length
+                total = 0
+            }
+            if (min === n.length) {
+                total += 1
+            }
+        }
+    }
 }
 
 console.log(total)
