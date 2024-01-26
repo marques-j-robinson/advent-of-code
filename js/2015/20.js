ed76c6fa8f7b4c50b1d187d1f4b068544d7d0eb8
marques-j-robinson <marques.j.robinson@gmail.com>
2024-01-26 02:53:19 -0800

    solve 2015 day 20 part 2

--- js/2015/20.js
+++ js/2015/20.js
@@ -1,9 +1,27 @@
 import {input} from '../puzzle-input.js'
 
+const part2 = true
+
+if (!part2) {
+    const houses = Array(Number(input)/10+1).fill(0)
+    for (let elf = 1; elf < houses.length; elf++) {
+        for (let house = elf; house < houses.length; house+=elf) {
+            houses[house] += elf*10
+        }
+    }
+
+    for (let house = 1; house < houses.length; house++) {
+        if (houses[house] > Number(input)) {
+            console.log(house)
+            break
+        }
+    }
+}
+
 const houses = Array(Number(input)/10+1).fill(0)
 for (let elf = 1; elf < houses.length; elf++) {
-    for (let house = elf; house < houses.length; house+=elf) {
-        houses[house] += elf*10
+    for (let house = elf, n = 0; house < houses.length && n < 50; house+=elf, n++) {
+        houses[house] += elf*11
     }
 }
 
