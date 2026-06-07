d1410315d0f0121703e9abf33122c22dc07114c4
marques-j-robinson <marques.j.robinson@gmail.com>
2026-06-07 10:38:12 -0700

    solve 2017 day 1 part 2

--- js/2017/1.js
+++ js/2017/1.js
@@ -1,13 +1,24 @@
 import { input } from '../puzzle-input.js'
 import { nums } from '../math.js'
 
+const part2 = true
+
 const list = nums(input.split(''))
 
+const getHalfwayIndex = (idx) => {
+  const offset = Math.floor(list.length / 2)
+  return (idx + offset) % list.length
+}
+
 const getPair = (idx) => {
-  if (idx === list.length - 1) {
-    return [list[idx], list[0]]
+  if (part2) {
+    return [list[idx], list[getHalfwayIndex(idx)]]
+  } else {
+    if (idx === list.length - 1) {
+      return [list[idx], list[0]]
+    }
+    return [list[idx], list[idx + 1]]
   }
-  return [list[idx], list[idx + 1]]
 }
 
 const checkMatch = (arr) => arr[0] === arr[1]
